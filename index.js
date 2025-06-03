9f7ae32e5aefc7b42426c9b88eb7fc338c5b5032
Shivani-Bajjuri <bajjurishivani30@gmail.com>
2025-06-03 11:32:11 +0530

    .

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors';
 const app = express();
 app.listen(8080, () => {
+  mongoose.connect("mongodb://localhost:27017/gcet");
   console.log('Server Started');
 });
+
+const userSchema = mongoose.Schema({
+  name: { type: String },
+});
+
+const user = mongoose.model('user', userSchema);
+
 app.use(cors());
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
  return res.send("Good Morning");
+});
+
+app.get("/register", async(req, res) => {
+ const result = await user.create({ name: "Shivani Bajjuri"});
+ return res.json(result);
 });
 
 app.get("/greet", (req,res) => {res.send("Greetings!")});
 
-app.get("/name", (req,res) => {res.send("Jaya Prakash")});
+app.get("/name", (req,res) => {res.send("Shivani Bajjuri")});
 
 app.get("/weather", (req,res) => {res.send("31°C")});
 
