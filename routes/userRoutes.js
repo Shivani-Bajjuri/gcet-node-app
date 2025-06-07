3495ec0bd83dddf8c560371ec71b485393970f90
Shivani-Bajjuri <bajjurishivani30@gmail.com>
2025-06-07 20:11:50 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userModel from "../models/userModel.js";
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const SECRET_KEY = "helloworld";
 
@@ -8,8 +8,8 @@ const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
-  const hashedPass = await bycrypt.hash(pass, 10);
-  const result = await userModel.insertOne({
+  const hashedPass = await bcrypt.hash(pass, 10);
+  const result = await userModel.create({
     name: name,
     email: email,
     pass: hashedPass,
@@ -21,7 +21,7 @@ userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
   const result = await userModel.findOne({ email: email });
   if (!result) return res.json({ message: "Invalid" });
-  const isPasswordValid = await bycrypt.compare(pass, result.pass);
+  const isPasswordValid = await bcrypt.compare(pass, result.pass);
   if (!isPasswordValid) return res.status(404).json({ message: "Invalid password" });
   const token = jwt.sign({email: result.email, id: result._id}, SECRET_KEY);
   console.log(result);
